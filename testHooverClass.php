529c5edc44a18bdc7cafce289905546d5fc380c1
lechuhuuha <59781669+lechuhuuha@users.noreply.github.com>
2021-04-12 17:02:45 +0700

    test crawler

--- testHooverClass.php
+++ testHooverClass.php
@@ -1,38 +1,63 @@
 <?php
 
+
 use function PHPSTORM_META\type;
 
-define('DEFAULT_URL', 'http://www.nettruyen.com/truyen-tranh/toi-la-tho-san-co-ki-nang-tu-sat-cap-sss/chap-33/703452');
+define('DEFAULT_URL', 'http://www.nettruyen.com/truyen-tranh/vo-dich-kiem-vuc/chap-118/703384');
 define('DEFAULT_TAG', 'img');
 require __DIR__ . '/Loader.php';
 \Autoload\Loader::init(__DIR__ . '/..');
 $vac = new Hoover();
-$newHtml = $vac->getContent(DEFAULT_URL);
-$tagA =  $vac->getTags(DEFAULT_URL, DEFAULT_TAG);
+// check to see the directory empty or not
+// $pid = basename($_GET["prodref"]); //let's sanitize it a bit
+// $dir = "/assets/$pid/v";
+
+// if (is_dir_empty($dir)) {
+//     echo "the folder is empty";
+// } else {
+//     echo "the folder is NOT empty";
+// }
+
+// function is_dir_empty($dir)
+// {
+//     if (!is_readable($dir)) return NULL;
+//     return (count(scandir($dir)) == 2);
+// }
+// check to see the directory empty or not
+$string = file_get_contents("./test.json");
+$json = json_decode($string);
+foreach ($json->data as $url) {
+    // $newHtml = $vac->getContent($url);
+    $tagA =  $vac->getTags($url, DEFAULT_TAG);
+    echo '<pre>';
+    preg_match('#<script type=\"application/ld\+json\">(.*?)</script>#is', file_get_contents($url), $match);
+    $stdClassObject = (json_decode($match[1]));
+    foreach ($stdClassObject as $key => $value) {
+        echo 'Key :' .  $key . PHP_EOL;
+        echo 'Value : ' . $value . PHP_EOL;
+        if ($key == 'headline') {
+            if (!file_exists($value)) {
+                mkdir('netComics/' . $value);
+            }
+            if (scandir($value)) {
+                netCrawler($tagA, $value);
+            }
+        }
+    }
+    echo '</pre>';
+}
+
+
+// $newHtml = $vac->getContent(DEFAULT_URL);
+// $tagA =  $vac->getTags(DEFAULT_URL, DEFAULT_TAG);
 
 // echo '<pre>';
 // print_r($newHtml);
 // echo '</pre>';
-echo '<pre>';
-preg_match('#<script type=\"application/ld\+json\">(.*?)</script>#is', file_get_contents(DEFAULT_URL), $match);
-$stdClassObject = (json_decode($match[1]));
-echo gettype($stdClassObject);
-foreach ($stdClassObject as $key => $value) {
-    echo 'Key :' .  $key . PHP_EOL;
-    echo 'Value : ' . $value . PHP_EOL;
-    if ($key == 'headline') {
-        if (!file_exists($value)) {
-            mkdir($value);
-        }
-        if (scandir($value)) {
-            netCrawler($tagA, $value);
-        }
-    }
-}
-echo '</pre>';
+
 
 // print_r($newHtml->documentElement);
-$nodeVal =  ($newHtml->documentElement->nodeValue);
+// $nodeVal =  ($newHtml->documentElement->nodeValue);
 // echo '<pre>';
 // echo  $nodeVal;
 
@@ -41,8 +66,8 @@ $nodeVal =  ($newHtml->documentElement->nodeValue);
 
 
 
-$url = strip_tags($_GET['url'] ?? DEFAULT_URL);
-$tag = strip_tags($_GET['tag'] ?? DEFAULT_TAG);
+// $url = strip_tags($_GET['url'] ?? DEFAULT_URL);
+// $tag = strip_tags($_GET['tag'] ?? DEFAULT_TAG);
 
 // echo 'DUMB OF TAGS : ' . PHP_EOL;
 // echo count($tagA);
@@ -55,8 +80,10 @@ function netCrawler($tagA, $dir)
         // preg_replace(array('/^\[/', '/\']$/'), '', $tagA[$i]['attributes']['src']);
         $netUrl = (trim($tagA[$i]['attributes']['src'], '//'));
         echo '<br>';
+        // $ch = curl_init($netUrl);
         $ch = curl_init($netUrl);
-        $fp = fopen($dir . '/trang '   . $i .  '.jpg', 'wb');
+        echo $netUrl;
+        $fp = fopen('netComics/' . $dir . '/trang '   . $i .  '.jpg', 'wb');
         curl_setopt($ch, CURLOPT_FILE, $fp);
         curl_setopt($ch, CURLOPT_HEADER, 0);
         curl_setopt($ch, CURLOPT_REFERER, 'http://www.nettruyen.com/');
